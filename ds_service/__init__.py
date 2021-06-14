fd07ad8e68fcfa69538c2b890a08fda96ec8a4ec
koromodako <koromodako@gmail.com>
2021-06-14 22:51:41 +0200

    raw and vmdk plugin first draft

--- ds_service/__init__.py
+++ ds_service/__init__.py
@@ -1,7 +1,7 @@
 """Datashark Service
 """
 from .__version__ import version, version_tuple
-from datashark_core.logging import LOGGING_MANAGER
+from ds_core.logging import LOGGING_MANAGER
 
 NAME = 'service'
 LOGGER = LOGGING_MANAGER.get_logger(NAME)
--- ds_service/main.py
+++ ds_service/main.py
@@ -4,21 +4,36 @@ import sys
 from argparse import ArgumentParser
 from aiohttp import web
 from aiohttp_basicauth import BasicAuthMiddleware
-from datashark_core import BANNER
-from datashark_core.config import CONFIG
+from ds_core import BANNER
+from ds_core.api import Artifact
+from ds_core.config import CONFIG
+from ds_core.plugin import load_installed_plugins
+from ds_core.logging import LOGGING_MANAGER
+from ds_core.dispatcher import (
+    DS_PLUGIN_JOBS,
+    DS_DISPATCH_JOBS,
+    enqueue_dispatch,
+)
 from . import LOGGER
 
 
 async def info(request):
-    return web.json_response({})
+    return web.json_response({
+        'plugin_job_count': len(DS_PLUGIN_JOBS.job_ids),
+        'dispatch_job_count': len(DS_DISPATCH_JOBS.job_ids),
+    })
 
 
 async def process(request):
-    return web.json_response({})
+    data = await request.json()
+    obj = Artifact(data['filepath'])
+    job = enqueue_dispatch(obj)
+    return web.json_response({'job': job.id})
 
 
 def parse_args():
     parser = ArgumentParser(description="Datashark Service")
+    parser.add_argument('--debug', '-d', action='store_true')
     parser.add_argument(
         '--bind',
         default=CONFIG.get_('datashark', 'service', 'bind', default='127.0.0.1'),
@@ -42,6 +57,8 @@ def app():
     """Application entry point"""
     LOGGER.info(BANNER)
     args = parse_args()
+    LOGGING_MANAGER.set_debug(args.debug)
+    load_installed_plugins()
     auth = BasicAuthMiddleware(username=args.user, password=args.pswd)
     webapp = web.Application(middlewares=[auth])
     webapp.add_routes([
