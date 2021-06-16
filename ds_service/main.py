c9124ac38ca6bc42ee4f41f223a059689320f5fd
koromodako <koromodako@gmail.com>
2021-06-17 00:35:51 +0200

    Update core and plugins

--- ds_service/main.py
+++ ds_service/main.py
@@ -7,7 +7,7 @@ from aiohttp import web
 from aiohttp_basicauth import BasicAuthMiddleware
 from ds_core import BANNER
 from ds_core.api import Artifact
-from ds_core.config import DSConfiguration
+from ds_core.config import DSConfiguration, DEFAULT_CONFIG_PATH
 from ds_core.plugin import load_installed_plugins
 from ds_core.logging import LOGGING_MANAGER
 from ds_core.dispatcher import (
@@ -19,10 +19,12 @@ from . import LOGGER
 
 
 async def info(request):
-    return web.json_response({
-        'plugin_job_count': len(DS_PLUGIN_JOBS.job_ids),
-        'dispatch_job_count': len(DS_DISPATCH_JOBS.job_ids),
-    })
+    return web.json_response(
+        {
+            'plugin_job_count': len(DS_PLUGIN_JOBS.job_ids),
+            'dispatch_job_count': len(DS_DISPATCH_JOBS.job_ids),
+        }
+    )
 
 
 async def process(request):
@@ -34,8 +36,16 @@ async def process(request):
 
 def parse_args():
     parser = ArgumentParser(description="Datashark Service")
-    parser.add_argument('--debug', '-d', action='store_true', help="Enable debugging")
-    parser.add_argument('config', type=Path, help="Configuration file")
+    parser.add_argument(
+        '--debug', '-d', action='store_true', help="Enable debugging"
+    )
+    parser.add_argument(
+        '--config',
+        '-c',
+        type=Path,
+        default=DEFAULT_CONFIG_PATH,
+        help="Configuration file",
+    )
     args = parser.parse_args()
     args.config = DSConfiguration(args.config)
     return args
@@ -50,10 +60,12 @@ def run(args):
     )
     webapp = web.Application(middlewares=[auth])
     webapp['config'] = args.config
-    webapp.add_routes([
-        web.get('/info', info),
-        web.post('/process', process),
-    ])
+    webapp.add_routes(
+        [
+            web.get('/info', info),
+            web.post('/process', process),
+        ]
+    )
     bind = args.config.get('datashark.service.bind', default='127.0.0.1')
     port = args.config.get('datashark.service.port', default=13740)
     LOGGER.info("starting on: %s:%s", bind, port)
