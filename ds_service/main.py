508e0dadc0d423ac4996bdf2843744519aca3633
koromodako <koromodako@gmail.com>
2021-06-16 18:41:52 +0200

    Update to use DSConfiguration

--- ds_service/main.py
+++ ds_service/main.py
@@ -1,12 +1,13 @@
 """Datashark CLI entry point
 """
 import sys
+from pathlib import Path
 from argparse import ArgumentParser
 from aiohttp import web
 from aiohttp_basicauth import BasicAuthMiddleware
 from ds_core import BANNER
 from ds_core.api import Artifact
-from ds_core.config import CONFIG
+from ds_core.config import DSConfiguration
 from ds_core.plugin import load_installed_plugins
 from ds_core.logging import LOGGING_MANAGER
 from ds_core.dispatcher import (
@@ -33,47 +34,39 @@ async def process(request):
 
 def parse_args():
     parser = ArgumentParser(description="Datashark Service")
-    parser.add_argument('--debug', '-d', action='store_true')
-    parser.add_argument(
-        '--bind',
-        default=CONFIG.get_('datashark', 'service', 'bind', default='127.0.0.1'),
+    parser.add_argument('--debug', '-d', action='store_true', help="Enable debugging")
+    parser.add_argument('config', type=Path, help="Configuration file")
+    args = parser.parse_args()
+    args.config = DSConfiguration(args.config)
+    return args
+
+
+def run(args):
+    LOGGING_MANAGER.set_debug(args.debug)
+    load_installed_plugins()
+    auth = BasicAuthMiddleware(
+        username=args.config.get('datashark', 'service', 'username', default='user'),
+        password=args.config.get('datashark', 'service', 'password'),
     )
-    parser.add_argument(
-        '--port',
-        default=CONFIG.get_('datashark', 'service', 'port', default=13740),
-    )
-    parser.add_argument(
-        '--user',
-        default=CONFIG.get_('datashark', 'service', 'user', default='user'),
-    )
-    parser.add_argument(
-        '--pswd',
-        default=CONFIG.get_('datashark', 'service', 'pswd'),
-    )
-    return parser.parse_args()
+    webapp = web.Application(middlewares=[auth])
+    webapp['config'] = args.config
+    webapp.add_routes([
+        web.get('/info', info),
+        web.post('/process', process),
+    ])
+    bind = args.config.get('datashark', 'service', 'bind', default='127.0.0.1')
+    port = args.config.get('datashark', 'service', 'port', default=13740)
+    LOGGER.info("starting on: %s:%s", bind, port)
+    web.run_app(webapp, host=bind, port=port, handle_signals=True)
 
 
 def app():
     """Application entry point"""
     LOGGER.info(BANNER)
     args = parse_args()
-    LOGGING_MANAGER.set_debug(args.debug)
-    load_installed_plugins()
-    auth = BasicAuthMiddleware(username=args.user, password=args.pswd)
-    webapp = web.Application(middlewares=[auth])
-    webapp.add_routes([
-        web.get('/info', info),
-        web.post('/process', process),
-    ])
-    LOGGER.info("starting on: %s:%s", args.bind, args.port)
     exitcode = 0
     try:
-        web.run_app(
-            webapp,
-            host=args.bind,
-            port=args.port,
-            handle_signals=True,
-        )
+        run(args)
     except:
         LOGGER.exception("unexpected exception!")
         exitcode = 1
