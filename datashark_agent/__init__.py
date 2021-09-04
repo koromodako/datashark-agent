1567f0057448a69c483409038f966b7b736ced37
koromodako <koromodako@gmail.com>
2021-09-05 01:06:34 +0200

    Update

--- datashark_agent/__init__.py
+++ datashark_agent/__init__.py
@@ -1,7 +1,7 @@
 """Datashark Service
 """
 from .__version__ import version, version_tuple
-from ds_core.logging import LOGGING_MANAGER
+from datashark_core.logging import LOGGING_MANAGER
 
 NAME = 'agent'
 LOGGER = LOGGING_MANAGER.get_logger(NAME)
