3a8c7fbbce6190485e3c222e8f85362cc275c161
koromodako <koromodako@gmail.com>
2021-10-04 23:34:08 +0200

    Update setup service

--- setup-service.ps1
+++ setup-service.ps1
@@ -1,9 +1,9 @@
 $params = @{
   Name = "DatasharkAgent"
-  BinaryPathName = '"C:\Users\user\AppData\Local\Python\Python39\Scripts\datashark-agent.exe --log-to C:\Users\user\AppData\Roaming\Datashark\Logs\ C:\Users\user\AppData\Roaming\Datashark\datashark.yml"'
+  BinaryPathName = '"C:\Users\user\AppData\Local\Programs\Python\Python39\Scripts\datashark-agent.exe" --log-to "C:\Users\user\AppData\Roaming\Datashark\Logs\" "C:\Users\user\AppData\Roaming\Datashark\datashark.yml"'
   Credential = ".\user"
   DisplayName = "Datashark Agent"
   StartupType = "Automatic"
-  Description = "Datashak agent service."
+  Description = "Datashak agent service"
 }
 New-Service @params
