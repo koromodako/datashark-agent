c706ba213d22e59beb5ec27ac818dc44c9c1aa8b
koromodako <koromodako@gmail.com>
2021-10-12 01:40:23 +0200

    Replace service setup scripts by full setup scripts

--- setup-service.ps1
+++
@@ -1,3 +0,0 @@
-$python = "Programs\Python\Python39\Scripts"
-$content = "%localappdata%\$python\datashark-agent.exe --log-to %appdata%\Datashark\Logs\ %appdata%\Datashark\datashark.yml"
-Set-Content -Path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\start-datashark-agent.cmd" -Value $content
